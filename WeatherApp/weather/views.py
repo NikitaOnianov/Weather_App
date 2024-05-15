ada904036172edf6c7e71995b89d14cfc338d3a2
NikitaOnianov <nikita.onianov@yandex.ru>
2024-05-15 21:31:04 +0300

    Ready

--- WeatherApp/weather/views.py
+++ WeatherApp/weather/views.py
@@ -1,12 +1,17 @@
 import requests
 from django.shortcuts import render
 from .models import City
+from .forms import CityForm
 
 def index(request):
     appid = '15d88b7896ee6a488eea948305e19db4'
     url = 'https://api.openweathermap.org/data/2.5/weather?q={}&units=metric&appid=' + appid
 
+    if(request.method == 'POST'):
+        form = CityForm(request.POST)
+        form.save()
 
+    form = CityForm()
 
 
     cities = City.objects.all()
@@ -23,6 +28,6 @@ def index(request):
 
         all_cities.append(city_info)
 
-    context = {'all_info': all_cities}
+    context = {'all_info': all_cities, 'form': form}
 
     return render(request, 'weather/index.html', context)
